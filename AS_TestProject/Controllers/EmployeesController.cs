d1a48ec8dd38ece7c87b1e7e7bc0d7a269484db3
Ryan Chapman <chapman.ryansadler@gmail.com>
2016-07-19 17:18:48 -0400

    Employee Edits in Progress
    plus polish on forms/gallery/etc

---
+++ AS_TestProject/Controllers/EmployeesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AS_TestProject.Entities;
+using AS_TestProject.Models;
+
+namespace AS_TestProject.Controllers
+{
+    public class EmployeesController : UserNames
+    {
+        private ReportEntities mb = new ReportEntities();
+
+        // GET: Employees/Create
+        [Authorize(Roles = "Admin, HR")]
+        public ActionResult Create()
+        {
+            ViewBag.PositionID = new SelectList(mb.Positions, "PositionID", "PositionName");
+            ViewBag.SiteID = new SelectList(mb.Sites, "SiteID", "SiteName");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "Admin, HR")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,FirstName,LastName,AddressLine1,AddressLine2,City,State,ZipCode,EmailAddress,HomePhone,CellPhone,BirthDate,HireDate,TerminationDate,IsManager,ManagerEmployeeID,PositionID,SiteID,IsActive,AddDate,AddByEmployeeID,RehireDate,FileNumber")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                mb.Employees.Add(employee);
+                mb.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PositionID = new SelectList(mb.Positions, "PositionID", "PositionName", employee.PositionID);
+            ViewBag.SiteID = new SelectList(mb.Sites, "SiteID", "SiteName", employee.SiteID);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        [Authorize(Roles = "Admin, HR")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = mb.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PositionID = new SelectList(mb.Positions, "PositionID", "PositionName", employee.PositionID);
+            ViewBag.SiteID = new SelectList(mb.Sites, "SiteID", "SiteName", employee.SiteID);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "Admin, HR")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,FirstName,LastName,AddressLine1,AddressLine2,City,State,ZipCode,EmailAddress,HomePhone,CellPhone,BirthDate,HireDate,TerminationDate,IsManager,ManagerEmployeeID,PositionID,SiteID,IsActive,AddDate,AddByEmployeeID,RehireDate,FileNumber")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                mb.Entry(employee).State = EntityState.Modified;
+                mb.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PositionID = new SelectList(mb.Positions, "PositionID", "PositionName", employee.PositionID);
+            ViewBag.SiteID = new SelectList(mb.Sites, "SiteID", "SiteName", employee.SiteID);
+            return View(employee);
+        }
+
+        //// GET: Employees/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Employee employee = mb.Employees.Find(id);
+        //    if (employee == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(employee);
+        //}
+
+        //// POST: Employees/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Employee employee = mb.Employees.Find(id);
+        //    mb.Employees.Remove(employee);
+        //    mb.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                mb.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
