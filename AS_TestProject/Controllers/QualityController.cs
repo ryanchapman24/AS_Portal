17024562b2231b2004e9c868d285fbd75db9ec8a
Ryan Chapman <chapman.ryansadler@gmail.com>
2016-11-03 13:30:30 -0400

    quality phase 1

--- AS_TestProject/Controllers/QualityController.cs
+++ AS_TestProject/Controllers/QualityController.cs
@@ -9,6 +9,7 @@ using Microsoft.Owin.Security;
 using AS_TestProject.Models;
 using AS_TestProject.Entities;
 using AS_TestProject.Models.Helpers;
+using System.Net;
 
 namespace AS_TestProject.Controllers
 {
@@ -17,10 +18,159 @@ namespace AS_TestProject.Controllers
     {
         private ReportEntities mb = new ReportEntities();
 
-        // GET: Quality
+        // GET: Quality/Index
         public ActionResult Index()
         {
             ViewBag.Domains = mb.DomainMasters.Where(d => d.IsActive == true).ToList();
+            return View();
+        }
+
+        // GET: Quality/Details
+        [Authorize(Roles = "Admin, Quality")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DomainMaster domain = mb.DomainMasters.Find(id);
+            if (domain == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Domain = domain.FileMask + " - " + domain.DomainName;
+
+            var mtgCFR = domain.CFRMortgages.Count;
+            var insCFR = domain.CFRInsurances.Count;
+            var prCFR = domain.CFRPatientRecruitments.Count;
+            decimal totCFR = mtgCFR + insCFR + prCFR;
+
+            //Telephone Etiquette % calculation
+            var mtgTE1 = domain.CFRMortgages.Where(d => d.TelephoneEtiquetteRating == 1).Count();
+            var mtgTE2 = domain.CFRMortgages.Where(d => d.TelephoneEtiquetteRating == 2).Count();
+            var mtgTE3 = domain.CFRMortgages.Where(d => d.TelephoneEtiquetteRating == 3).Count();
+
+            var insTE1 = domain.CFRInsurances.Where(d => d.TelephoneEtiquetteRating == 1).Count();
+            var insTE2 = domain.CFRInsurances.Where(d => d.TelephoneEtiquetteRating == 2).Count();
+            var insTE3 = domain.CFRInsurances.Where(d => d.TelephoneEtiquetteRating == 3).Count();
+
+            var prTE1 = domain.CFRPatientRecruitments.Where(d => d.TelephoneEtiquetteRating == 1).Count();
+            var prTE2 = domain.CFRPatientRecruitments.Where(d => d.TelephoneEtiquetteRating == 2).Count();
+            var prTE3 = domain.CFRPatientRecruitments.Where(d => d.TelephoneEtiquetteRating == 3).Count();
+
+            decimal totalTE1 = mtgTE1 + insTE1 + prTE1;
+            decimal totalTE2 = mtgTE2 + insTE2 + prTE2;
+            decimal totalTE3 = mtgTE3 + insTE3 + prTE3;
+
+            decimal percentTE1 = Math.Round((totalTE1 * 10000) / totCFR) / 100;
+            decimal percentTE2 = Math.Round((totalTE2 * 10000) / totCFR) / 100;
+            decimal percentTE3 = Math.Round((totalTE3 * 10000) / totCFR) / 100;
+
+            ViewBag.PercentTE1 = percentTE1;
+            ViewBag.PercentTE2 = percentTE2;
+            ViewBag.PercentTE3 = percentTE3;
+
+            //Professionalism % calculation
+            var mtgP1 = domain.CFRMortgages.Where(d => d.ProfessionalismRating == 1).Count();
+            var mtgP2 = domain.CFRMortgages.Where(d => d.ProfessionalismRating == 2).Count();
+            var mtgP3 = domain.CFRMortgages.Where(d => d.ProfessionalismRating == 3).Count();
+
+            var insP1 = domain.CFRInsurances.Where(d => d.ProfessionalismRating == 1).Count();
+            var insP2 = domain.CFRInsurances.Where(d => d.ProfessionalismRating == 2).Count();
+            var insP3 = domain.CFRInsurances.Where(d => d.ProfessionalismRating == 3).Count();
+
+            var prP1 = domain.CFRPatientRecruitments.Where(d => d.ProfessionalismRating == 1).Count();
+            var prP2 = domain.CFRPatientRecruitments.Where(d => d.ProfessionalismRating == 2).Count();
+            var prP3 = domain.CFRPatientRecruitments.Where(d => d.ProfessionalismRating == 3).Count();
+
+            decimal totalP1 = mtgP1 + insP1 + prP1;
+            decimal totalP2 = mtgP2 + insP2 + prP2;
+            decimal totalP3 = mtgP3 + insP3 + prP3;
+
+            decimal percentP1 = Math.Round((totalP1 * 10000) / totCFR) / 100;
+            decimal percentP2 = Math.Round((totalP2 * 10000) / totCFR) / 100;
+            decimal percentP3 = Math.Round((totalP3 * 10000) / totCFR) / 100;
+
+            ViewBag.PercentP1 = percentP1;
+            ViewBag.PercentP2 = percentP2;
+            ViewBag.PercentP3 = percentP3;
+
+            //Compliance % calculation
+            var mtgC1 = domain.CFRMortgages.Where(d => d.ComplianceRating == 1).Count();
+            var mtgC2 = domain.CFRMortgages.Where(d => d.ComplianceRating == 2).Count();
+            var mtgC3 = domain.CFRMortgages.Where(d => d.ComplianceRating == 3).Count();
+
+            var insC1 = domain.CFRInsurances.Where(d => d.ComplianceRating == 1).Count();
+            var insC2 = domain.CFRInsurances.Where(d => d.ComplianceRating == 2).Count();
+            var insC3 = domain.CFRInsurances.Where(d => d.ComplianceRating == 3).Count();
+
+            var prC1 = domain.CFRPatientRecruitments.Where(d => d.ComplianceRating == 1).Count();
+            var prC2 = domain.CFRPatientRecruitments.Where(d => d.ComplianceRating == 2).Count();
+            var prC3 = domain.CFRPatientRecruitments.Where(d => d.ComplianceRating == 3).Count();
+
+            decimal totalC1 = mtgC1 + insC1 + prC1;
+            decimal totalC2 = mtgC2 + insC2 + prC2;
+            decimal totalC3 = mtgC3 + insC3 + prC3;
+
+            decimal percentC1 = Math.Round((totalC1 * 10000) / totCFR) / 100;
+            decimal percentC2 = Math.Round((totalC2 * 10000) / totCFR) / 100;
+            decimal percentC3 = Math.Round((totalC3 * 10000) / totCFR) / 100;
+
+            ViewBag.PercentC1 = percentC1;
+            ViewBag.PercentC2 = percentC2;
+            ViewBag.PercentC3 = percentC3;
+
+            //Adherence % calculation
+            var mtgA1 = domain.CFRMortgages.Where(d => d.AdheranceRating == 1).Count();
+            var mtgA2 = domain.CFRMortgages.Where(d => d.AdheranceRating == 2).Count();
+            var mtgA3 = domain.CFRMortgages.Where(d => d.AdheranceRating == 3).Count();
+
+            var insA1 = domain.CFRInsurances.Where(d => d.AdheranceRating == 1).Count();
+            var insA2 = domain.CFRInsurances.Where(d => d.AdheranceRating == 2).Count();
+            var insA3 = domain.CFRInsurances.Where(d => d.AdheranceRating == 3).Count();
+
+            var prA1 = domain.CFRPatientRecruitments.Where(d => d.AdheranceRating == 1).Count();
+            var prA2 = domain.CFRPatientRecruitments.Where(d => d.AdheranceRating == 2).Count();
+            var prA3 = domain.CFRPatientRecruitments.Where(d => d.AdheranceRating == 3).Count();
+
+            decimal totalA1 = mtgA1 + insA1 + prA1;
+            decimal totalA2 = mtgA2 + insA2 + prA2;
+            decimal totalA3 = mtgA3 + insA3 + prA3;
+
+            decimal percentA1 = Math.Round((totalA1 * 10000) / totCFR) / 100;
+            decimal percentA2 = Math.Round((totalA2 * 10000) / totCFR) / 100;
+            decimal percentA3 = Math.Round((totalA3 * 10000) / totCFR) / 100;
+
+            ViewBag.PercentA1 = percentA1;
+            ViewBag.PercentA2 = percentA2;
+            ViewBag.PercentA3 = percentA3;
+
+            //Accuracy of Information % calculation
+            var mtgAOI1 = domain.CFRMortgages.Where(d => d.AccuracyOfInformationRating == 1).Count();
+            var mtgAOI2 = domain.CFRMortgages.Where(d => d.AccuracyOfInformationRating == 2).Count();
+            var mtgAOI3 = domain.CFRMortgages.Where(d => d.AccuracyOfInformationRating == 3).Count();
+
+            var insAOI1 = domain.CFRInsurances.Where(d => d.AccuracyOfInformationRating == 1).Count();
+            var insAOI2 = domain.CFRInsurances.Where(d => d.AccuracyOfInformationRating == 2).Count();
+            var insAOI3 = domain.CFRInsurances.Where(d => d.AccuracyOfInformationRating == 3).Count();
+
+            var prAOI1 = domain.CFRPatientRecruitments.Where(d => d.AccuracyOfInformationRating == 1).Count();
+            var prAOI2 = domain.CFRPatientRecruitments.Where(d => d.AccuracyOfInformationRating == 2).Count();
+            var prAOI3 = domain.CFRPatientRecruitments.Where(d => d.AccuracyOfInformationRating == 3).Count();
+
+            decimal totalAOI1 = mtgAOI1 + insAOI1 + prAOI1;
+            decimal totalAOI2 = mtgAOI2 + insAOI2 + prAOI2;
+            decimal totalAOI3 = mtgAOI3 + insAOI3 + prAOI3;
+
+            decimal percentAOI1 = Math.Round((totalAOI1 * 10000) / totCFR) / 100;
+            decimal percentAOI2 = Math.Round((totalAOI2 * 10000) / totCFR) / 100;
+            decimal percentAOI3 = Math.Round((totalAOI3 * 10000) / totCFR) / 100;
+
+            ViewBag.PercentAOI1 = percentAOI1;
+            ViewBag.PercentAOI2 = percentAOI2;
+            ViewBag.PercentAOI3 = percentAOI3;
+
             return View();
         }
     }
