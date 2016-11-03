9aad73e9a297a0be0b504bae614ad6dbb27f32e7
Ryan Chapman <chapman.ryansadler@gmail.com>
2016-11-03 03:21:52 -0400

    Quality Index Shell

---
+++ AS_TestProject/Controllers/QualityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using AS_TestProject.Models;
+using AS_TestProject.Entities;
+using AS_TestProject.Models.Helpers;
+
+namespace AS_TestProject.Controllers
+{
+    [Authorize(Roles = "Quality")]
+    public class QualityController : UserNames
+    {
+        private ReportEntities mb = new ReportEntities();
+
+        // GET: Quality
+        public ActionResult Index()
+        {
+            ViewBag.Domains = mb.DomainMasters.Where(d => d.IsActive == true).ToList();
+            return View();
+        }
+    }
+}
