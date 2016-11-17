6ea00d8b7fb2abe76519b810fe33fbc94d8a0b67
Ryan Chapman <chapman.ryansadler@gmail.com>
2016-11-17 11:41:39 -0500

    Sisense Controller and view

---
+++ AS_TestProject/Controllers/SisenseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AS_TestProject.Entities;
+using AS_TestProject.Models;
+using Microsoft.AspNet.Identity;
+using System.IO;
+
+namespace AS_TestProject.Controllers
+{
+    public class SisenseController : UserNames
+    {
+        // GET: Sisense
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
