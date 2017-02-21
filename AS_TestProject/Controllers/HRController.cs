1f6d484b9690602f45a495f38310f28f778df02e
Ryan Chapman <chapman.ryansadler@gmail.com>
2017-02-21 16:32:47 -0500

    swapped HR & IT notification logic bc it was backwards....
    damnit

--- AS_TestProject/Controllers/HRController.cs
+++ AS_TestProject/Controllers/HRController.cs
@@ -56,7 +56,7 @@ namespace AS_TestProject.Controllers
                 db.Documents.Add(document);
                 db.SaveChanges();
 
-                foreach (var HRuser in db.Users.Where(u => u.Roles.Any(r => r.RoleId == "0f471264-3ee2-473d-9fb8-e1ac8b6e01b8")).ToList())
+                foreach (var HRuser in db.Users.Where(u => u.Roles.Any(r => r.RoleId == "cf0c9cdc-c2d7-4abf-9da7-72b5d4245348")).ToList())
                 {
                     Notification n = new Notification()
                     {
