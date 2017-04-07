bca0eb4fa4b21b1e1917a4eee56d34c66e9d645a
Ryan Chapman <chapman.ryansadler@gmail.com>
2017-04-07 15:22:29 -0400

    publish update

--- AS_TestProject/obj/Release/Package/PackageTmp/assets/js/morris-conf.js
+++ AS_TestProject/obj/Release/Package/PackageTmp/assets/js/morris-conf.js
@@ -81,6 +81,24 @@ var Script = function () {
         barColors: ['#ac92ec']
       });
 
+      Morris.Bar({
+          element: 'hero-bar2',
+          data: [
+            { device: 'iPhone', geekbench: 136 },
+            { device: 'iPhone 3G', geekbench: 137 },
+            { device: 'iPhone 3GS', geekbench: 275 },
+            { device: 'iPhone 4', geekbench: 380 },
+            { device: 'iPhone 4S', geekbench: 655 },
+            { device: 'iPhone 5', geekbench: 1571 }
+          ],
+          xkey: 'device',
+          ykeys: ['geekbench'],
+          labels: ['Geekbench'],
+          barRatio: 0.4,
+          xLabelAngle: 35,
+          hideHover: 'auto',
+          barColors: ['#ac92ec']
+      });
       new Morris.Line({
         element: 'examplefirst',
         xkey: 'year',
