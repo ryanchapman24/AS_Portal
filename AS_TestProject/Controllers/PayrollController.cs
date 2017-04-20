0d25d83e859e08d75626f81d4d1c2d9dda3c30ed
Ryan Chapman <chapman.ryansadler@gmail.com>
2017-04-20 11:07:50 -0400

    revert my project back to old payroll and deploy

--- AS_TestProject/Controllers/PayrollController.cs
+++ AS_TestProject/Controllers/PayrollController.cs
@@ -23,24 +23,18 @@ namespace AS_TestProject.Controllers
             public string FileMaskPlusName { get; set; }
         }
 
-        public class Skill
-        {
-            public int Id { get; set; }
-            public string FileMaskPlusName { get; set; }
-        }
-
         // GET: Payroll
         [Authorize]
         public ActionResult Index(int id)
         {
             var user = db.Users.Find(User.Identity.GetUserId());
 
-            if (id != user.EmployeeID  && !(User.IsInRole("Payroll")))
+            if (id != user.EmployeeID && !(User.IsInRole("Payroll")))
             {
                 return RedirectToAction("Directory", "Home");
             }
 
-            var now = System.DateTime.Now;;
+            var now = System.DateTime.Now; ;
             var payPeriod = mb.PayPeriods.First(p => p.StartDate <= now && System.Data.Entity.DbFunctions.AddDays(p.EndDate, 1) > now);
             var payPeriodId = payPeriod.PayPeriodID;
             var prevPayPeriodId = payPeriodId - 1;
@@ -74,25 +68,8 @@ namespace AS_TestProject.Controllers
             }
 
             ViewBag.DomainMasterID = new SelectList(domains, "Id", "FileMaskPlusName");
+            //ViewBag.DomainMasterID = new SelectList(mb.DomainMasters, "DomainMasterID", "DomainName");
             ViewBag.AgentTimeAdjustmentReasonID = new SelectList(mb.AgentTimeAdjustmentReasons, "AgentTimeAdjustmentReasonID", "Reason");
-
-            //Prep for Payroll Changes
-            ViewBag.Domains = new MultiSelectList(domains, "Id", "FileMaskPlusName");
-            var allAgentHours = mb.AgentDailyHours.Where(a => a.EmployeeID == id && (a.PayPeriodID == payPeriodId || a.PayPeriodID == prevPayPeriodId)).Include(a => a.DomainMaster).Include(a => a.Employee).Include(a => a.AgentTimeAdjustmentReason).Include(a => a.PayPeriod).OrderByDescending(a => a.LoginTimeStamp).ToList();
-            var agentHoursToDomainMasters = mb.AgentDailyHoursToDomainMasters.ToList();
-            var relatedSkills = agentHoursToDomainMasters.Where(a => allAgentHours.Any(h => h.AgentDailyHoursID == a.AgentDailyHoursID)).ToList();
-            var skills = new List<Skill>();
-            foreach (var skill in relatedSkills)
-            {
-                var newSkill = new Skill();
-                newSkill.Id = skill.AgentDailyHoursID;
-                var fileMaskPlusName = mb.DomainMasters.FirstOrDefault(d => d.DomainMasterID == skill.DomainMasterID).FileMask + " - " + mb.DomainMasters.FirstOrDefault(d => d.DomainMasterID == skill.DomainMasterID).DomainName;
-                newSkill.FileMaskPlusName = fileMaskPlusName;
-
-                skills.Add(newSkill);
-            }
-            ViewBag.Skills = skills.OrderBy(s => s.FileMaskPlusName);
-            //End Prep
 
             return View(agentDailyHours);
         }
@@ -103,7 +80,7 @@ namespace AS_TestProject.Controllers
         [HttpPost]
         [Authorize(Roles = "Admin, Payroll")]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "AgentDailyHoursID,EmployeeID,DomainMasterID,LoginTimeStamp,LogoutTimeStamp,LoginDuration,AgentTimeAdjustmentReasonID,PayPeriodID,EditByEmployeeID,EditTimeStamp")] AgentDailyHour agentDailyHour, int empId, short ppId, List<byte> Domains)
+        public ActionResult Create([Bind(Include = "AgentDailyHoursID,EmployeeID,DomainMasterID,LoginTimeStamp,LogoutTimeStamp,LoginDuration,AgentTimeAdjustmentReasonID,PayPeriodID,EditByEmployeeID,EditTimeStamp")] AgentDailyHour agentDailyHour, int empId, short ppId)
         {
             if (ModelState.IsValid)
             {
@@ -111,28 +88,12 @@ namespace AS_TestProject.Controllers
 
                 if (agentDailyHour.LoginTimeStamp != agentDailyHour.LogoutTimeStamp)
                 {
-                    agentDailyHour.DomainMasterID = 21;
                     agentDailyHour.PayPeriodID = ppId;
                     agentDailyHour.EmployeeID = empId;
                     agentDailyHour.EditByEmployeeID = user.EmployeeID;
                     agentDailyHour.EditTimeStamp = System.DateTime.Now;
                     mb.AgentDailyHours.Add(agentDailyHour);
                     mb.SaveChanges();
-
-                    if (Domains != null)
-                    {
-                        int count = Domains.Count();
-                        for (byte i = 0; i < count; i++)
-                        {
-                            var d = new AgentDailyHoursToDomainMaster();
-                            d.AgentDailyHoursID = agentDailyHour.AgentDailyHoursID;
-                            d.DomainMasterID = Domains[i];
-                            d.IsDeleted = false;
-                            mb.AgentDailyHoursToDomainMasters.Add(d);
-                            mb.SaveChanges();
-                        }
-                        mb.SaveChanges();
-                    }
 
                     // STORED PROCEDURES
                     mb.uspHoursCalculation(empId, ppId);
@@ -153,13 +114,10 @@ namespace AS_TestProject.Controllers
             }
 
             ViewBag.DomainMasterID = new SelectList(domains, "Id", "FileMaskPlusName");
+            //ViewBag.DomainMasterID = new SelectList(mb.DomainMasters, "DomainMasterID", "DomainName", agentDailyHour.DomainMasterID);
             ViewBag.AgentTimeAdjustmentReasonID = new SelectList(mb.AgentTimeAdjustmentReasons, "AgentTimeAdjustmentReasonID", "Reason");
 
-            //Prep for Payroll Changes
-            ViewBag.Domains = new MultiSelectList(domains, "Id", "FileMaskPlusName");
-            //End Prep
-
-            return RedirectToAction("Index", new { id = empId });            
+            return RedirectToAction("Index", new { id = empId });
         }
 
         // GET: Payroll/Edit/5
@@ -177,38 +135,10 @@ namespace AS_TestProject.Controllers
             {
                 return HttpNotFound();
             }
-
             ViewBag.DomainMasterID = new SelectList(mb.DomainMasters, "DomainMasterID", "DomainName", agentDailyHour.DomainMasterID);
             ViewBag.EmployeeID = new SelectList(mb.Employees, "EmployeeID", "FirstName", agentDailyHour.EmployeeID);
             ViewBag.AgentTimeAdjustmentReasonID = new SelectList(mb.AgentTimeAdjustmentReasons, "AgentTimeAdjustmentReasonID", "Reason", agentDailyHour.AgentTimeAdjustmentReasonID);
             ViewBag.PayPeriodID = new SelectList(mb.PayPeriods, "PayPeriodID", "PayPeriodID", agentDailyHour.PayPeriodID);
-
-            //Prep for Payroll Changes
-            var domains = new List<Domain>();
-            foreach (var domain in mb.DomainMasters.Where(d => d.IsActive == true).OrderBy(d => d.FileMask))
-            {
-                var selection = new Domain();
-                selection.Id = domain.DomainMasterID;
-                selection.FileMaskPlusName = domain.FileMask + " - " + domain.DomainName;
-
-                domains.Add(selection);
-            }
-            var domains2 = mb.DomainMasters.Where(d => d.DomainMasterID == agentDailyHour.DomainMasterID).ToArray();
-            ViewBag.Domains = new MultiSelectList(domains, "Id", "FileMaskPlusName", domains2);
-            var relatedSkills = mb.AgentDailyHoursToDomainMasters.Where(h => h.AgentDailyHoursID == id).ToList();
-            var skills = new List<Skill>();
-            foreach (var skill in relatedSkills)
-            {
-                var newSkill = new Skill();
-                newSkill.Id = skill.AgentDailyHoursID;
-                var fileMaskPlusName = mb.DomainMasters.FirstOrDefault(d => d.DomainMasterID == skill.DomainMasterID).FileMask + " - " + mb.DomainMasters.FirstOrDefault(d => d.DomainMasterID == skill.DomainMasterID).DomainName;
-                newSkill.FileMaskPlusName = fileMaskPlusName;
-
-                skills.Add(newSkill);
-            }
-            ViewBag.Skills = skills.OrderBy(s => s.FileMaskPlusName);
-            //End Prep
-
             return View(agentDailyHour);
         }
 
@@ -226,19 +156,8 @@ namespace AS_TestProject.Controllers
 
                 if (agentDailyHour.LoginTimeStamp == agentDailyHour.LogoutTimeStamp)
                 {
-                    var empId = agentDailyHour.EmployeeID;
-                    var ppId = agentDailyHour.PayPeriodID;
                     mb.AgentDailyHours.Attach(agentDailyHour);
                     mb.AgentDailyHours.Remove(agentDailyHour);
-                    foreach (var skill in mb.AgentDailyHoursToDomainMasters.Where(s => s.AgentDailyHoursID == agentDailyHour.AgentDailyHoursID).ToList())
-                    {
-                        mb.AgentDailyHoursToDomainMasters.Remove(skill);
-                        mb.SaveChanges();
-                    }
-                    mb.SaveChanges();
-
-                    // STORED PROCEDURES
-                    mb.uspHoursCalculation(empId, ppId);
                     mb.SaveChanges();
                 }
                 else
@@ -256,41 +175,15 @@ namespace AS_TestProject.Controllers
                     mb.uspHoursCalculation(agentDailyHour.EmployeeID, agentDailyHour.PayPeriodID);
                     mb.SaveChanges();
                 }
-               
+
                 return RedirectToAction("Index", new { id = agentDailyHour.EmployeeID });
             }
             ViewBag.DomainMasterID = new SelectList(mb.DomainMasters, "DomainMasterID", "DomainName", agentDailyHour.DomainMasterID);
             ViewBag.EmployeeID = new SelectList(mb.Employees, "EmployeeID", "FirstName", agentDailyHour.EmployeeID);
             ViewBag.AgentTimeAdjustmentReasonID = new SelectList(mb.AgentTimeAdjustmentReasons, "AgentTimeAdjustmentReasonID", "Reason", agentDailyHour.AgentTimeAdjustmentReasonID);
             ViewBag.PayPeriodID = new SelectList(mb.PayPeriods, "PayPeriodID", "PayPeriodID", agentDailyHour.PayPeriodID);
-
-            //Prep for Payroll Changes
-            var domains = new List<Domain>();
-            foreach (var domain in mb.DomainMasters.Where(d => d.IsActive == true).OrderBy(d => d.FileMask))
-            {
-                var selection = new Domain();
-                selection.Id = domain.DomainMasterID;
-                selection.FileMaskPlusName = domain.FileMask + " - " + domain.DomainName;
-
-                domains.Add(selection);
-            }
-            var domains2 = mb.DomainMasters.Where(d => d.DomainMasterID == agentDailyHour.DomainMasterID).ToArray();
-            ViewBag.Domains = new MultiSelectList(domains, "Id", "FileMaskPlusName", domains2);
-            var relatedSkills = mb.AgentDailyHoursToDomainMasters.Where(h => h.AgentDailyHoursID == agentDailyHour.AgentDailyHoursID).ToList();
-            var skills = new List<Skill>();
-            foreach (var skill in relatedSkills)
-            {
-                var newSkill = new Skill();
-                newSkill.Id = skill.AgentDailyHoursID;
-                var fileMaskPlusName = mb.DomainMasters.FirstOrDefault(d => d.DomainMasterID == skill.DomainMasterID).FileMask + " - " + mb.DomainMasters.FirstOrDefault(d => d.DomainMasterID == skill.DomainMasterID).DomainName;
-                newSkill.FileMaskPlusName = fileMaskPlusName;
-
-                skills.Add(newSkill);
-            }
-            ViewBag.Skills = skills.OrderBy(s => s.FileMaskPlusName);
-            //End Prep
-
             return View(agentDailyHour);
         }
     }
 }
+
